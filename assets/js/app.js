2609afc40cabe62ee8034eb3f1c99242ab37bb2e
NicolasMica <nicolas@micallef.pro>
2018-06-07 11:44:20 +0200

    Updated templating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import App from './components/App.vue'
 
 document.addEventListener('deviceready', () => {
 
     window.events = new Vue
 
     const app = new Vue({
+        components: { App },
         el: '#root',
         template: '<App/>'
     })
