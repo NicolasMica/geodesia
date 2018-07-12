91fcb8a9c58dddc3ec87b8edc87dcfd51c549ca6
NicolasMica <nicolas@micallef.pro>
2018-07-12 11:01:25 +0200

    Added project related routes

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,21 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 Vue.use(Router)
 
 import Releve from './components/Releve'
-import Home from './pages/Home.vue'
+import Login from './pages/Auth/Login.vue'
+import ProjectIndex from './pages/Projects/Index.vue'
+import ProjectEdit from './pages/Projects/Edit.vue'
+import MarkerEdit from './pages/Markers/Edit.vue'
 
 export default new Router({
     routes: [
         {
             path: '/',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/projects',
+            name: 'projects.index',
+            component: ProjectIndex,
+        },
+        {
+            path: '/projects/:id/edit',
+            name: 'projects.edit',
+            component: ProjectEdit,
+            props: true
+        },
+        {
+            path: '/projects/:project_id/markers/:marker_id',
+            name: 'markers.edit',
+            component: MarkerEdit,
+            props: true
+        },
+        {
+            path: '/releve',
             name: 'Releve',
             component: Releve
         },
-        {
-            path: '/mockup',
-            name: 'Home',
-            component: Home
-        }
     ]
 })
