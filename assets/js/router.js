98c504975be3c9ab80760b99fcadc5bdeef943e9
NicolasMica <nicolas@micallef.pro>
2018-07-12 15:03:04 +0200

    Added ProjectCreate endpoint

--- assets/js/router.js
+++ assets/js/router.js
@@ -6,6 +6,7 @@ Vue.use(Router)
 import Releve from './components/Releve'
 import Login from './pages/Auth/Login.vue'
 import ProjectIndex from './pages/Projects/Index.vue'
+import ProjectCreate from './pages/Projects/Create.vue'
 import ProjectEdit from './pages/Projects/Edit.vue'
 import MarkerEdit from './pages/Markers/Edit.vue'
 
@@ -20,6 +21,11 @@ export default new Router({
             path: '/projects',
             name: 'projects.index',
             component: ProjectIndex,
+        },
+        {
+            path: '/projects/create',
+            name: 'projects.create',
+            component: ProjectCreate,
         },
         {
             path: '/projects/:id/edit',
