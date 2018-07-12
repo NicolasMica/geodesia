e94b99c5e1c961cd513e7fcfc5ac9bcbba145bfe
NicolasMica <nicolas@micallef.pro>
2018-07-12 10:59:03 +0200

    Extracted axios to store

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,13 @@
 import Vue from 'vue'
-import axios from 'axios'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 
-require('ol/ol.css');
-
-const objectToFormData = (obj, form, namespace) => {
-    let fd = form || new FormData()
-    let formKey
-
-    for(let property in obj) {
-        if(obj.hasOwnProperty(property)) {
-
-            if(namespace) {
-                formKey = namespace + '[' + property + ']'
-            } else {
-                formKey = property
-            }
-
-            // if the property is an object, but not a File,
-            // use recursivity.
-            if(typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-                objectToFormData(obj[property], fd, property)
-            } else {
-                // if it's a string or a File object
-                fd.append(formKey, obj[property])
-            }
-        }
-    }
-
-    return fd;
-}
+require('ol/ol.css')
 
 document.addEventListener('deviceready', () => {
 
     window.events = new Vue
-
-    window.axios = axios.create({
-        baseURL: window.Api.path,
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
-        },
-        transformResponse: [
-            (data, headers) => {
-                headers['Content-Type'] = 'multipart/form-data'
-
-                return objectToFormData(data)
-            }
-        ],
-        auth: window.Api.auth,
-        withCredentials: true
-    })
 
     const app = new Vue({
         components: { App },
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 import Releve from './stores/Releve'
 import Chantier from './stores/Chantier'
+
 Vue.use(Vuex)
+
+const api = axios.create({
+    baseURL: window.Api.path,
+    headers: {
+        // 'X-Requested-With': 'XMLHttpRequest',
+        // 'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
+    }
+})
 
 const store = new Vuex.Store({
     namespaced: true,
